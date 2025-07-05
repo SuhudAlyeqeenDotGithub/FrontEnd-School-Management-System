4883ae43e5ff1985d357f0bc9016695873b73218
SuhudAlyeqeenDotGithub <your_email@example.com>
2025-07-05 20:19:51 +0100

    Handled editing qualification

--- src/lib/component/staff/editStaffComp.tsx
+++ src/lib/component/staff/editStaffComp.tsx
@@ -20,172 +20,9 @@ import ImageUploadDiv from "../general/imageUploadCom";
 import { handleApiRequest } from "@/axios/axiosClient";
 import axios from "axios";
 import { createStaffProfile } from "@/redux/features/staff/staffThunks";
+import { QualificationDialog } from "./newStaffComp";
 
-export const QualificationDialog = ({
-  type = "new",
-  data,
-  onSave,
-  onClose
-}: {
-  type?: string;
-  data: QualificationType;
-  onSave: (save: boolean, returnData: QualificationType) => void;
-  onClose: (close: boolean) => void;
-}) => {
-  const newDialog = type === "new";
-  const { handleUnload } = useNavigationHandler();
-  const [unsaved, setUnsaved] = useState(false);
-  const [error, setError] = useState("");
-  const [openUnsavedDialog, setOpenUnsavedDialog] = useState(false);
-  const [localData, setLocalData] = useState({
-    _id: data._id,
-    qualificationName: data.qualificationName,
-    schoolName: data.schoolName,
-    startDate: data.startDate,
-    endDate: data.endDate
-  });
-
-  const { qualificationName, schoolName, startDate, endDate } = localData;
-
-  useEffect(() => {
-    if (!unsaved) return;
-    handleUnload("add");
-
-    return () => {
-      handleUnload("remove");
-    };
-  }, [unsaved]);
-
-  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setUnsaved(true);
-    setLocalData((prev: any) => ({ ...prev, [e.target.name]: e.target.value }));
-  };
-
-  const validationPassed = () => {
-    if (!qualificationName) {
-      setError("Missing Data: Please provide a qualification name");
-      return false;
-    }
-
-    if (!schoolName) {
-      setError("Missing Data: Please provide a school/institution name");
-      return false;
-    }
-
-    if (!startDate) {
-      setError("Missing Data: Please provide a qualification start date");
-      return false;
-    }
-    if (!endDate) {
-      setError("Missing Data: Please provide a qualification end date");
-      return false;
-    }
-
-    return true;
-  };
-  return (
-    <div className="flex justify-center items-center absolute bg-foregroundColor-70 inset-0">
-      <ContainerComponent style="w-[55%] h-[55%] gap-10 flex flex-col z-40 bg-backgroundColor overflow-auto">
-        {error && (
-          <ErrorDiv
-            onClose={(close) => {
-              if (close) {
-                setError("");
-              }
-            }}
-          >
-            {error}
-          </ErrorDiv>
-        )}
-        {openUnsavedDialog && (
-          <YesNoDialog
-            warningText="You have unsaved changes. Are you sure you want to proceed?"
-            onNo={() => {
-              const container = document.getElementById("staffDialogContainer");
-              if (container) {
-                container.style.overflow = "";
-              }
-              setOpenUnsavedDialog(false);
-            }}
-            onYes={() => {
-              handleUnload("remove");
-              const container = document.getElementById("staffDialogContainer");
-              if (container) {
-                container.style.overflow = "";
-              }
-              onClose(true);
-            }}
-          />
-        )}
-        <div className="flex justify-between items-center">
-          <h2>{newDialog ? "New" : "Edit"} Qualification</h2>
-          <div className="flex justify-between items-center gap-5">
-            <LoaderButton
-              buttonText="Save"
-              loadingButtonText="Saving..."
-              disabled={!unsaved}
-              buttonStyle="w-full"
-              onClick={async () => {
-                if (validationPassed()) {
-                  setError("");
-                  onSave(true, { ...localData, _id: type === "edit" ? localData._id : nanoid() });
-                }
-              }}
-            />
-            <IoClose
-              onClick={() => {
-                if (!unsaved) {
-                  onClose(true);
-                } else {
-                  const container = document.getElementById("staffDialogContainer");
-                  if (container) {
-                    container.style.overflow = "hidden";
-                  }
-                  setOpenUnsavedDialog(true);
-                }
-              }}
-              className="text-[50px] hover:text-foregroundColor-50 hover:cursor-pointer"
-            />
-          </div>
-        </div>
-        <div className="flex gap-3 flex-col">
-          <InputComponent
-            placeholder="Qualification Name *"
-            required
-            name="qualificationName"
-            value={qualificationName}
-            onChange={handleInputChange}
-          />
-          <InputComponent
-            placeholder="School Name *"
-            required
-            name="schoolName"
-            value={schoolName}
-            onChange={handleInputChange}
-          />
-          <InputComponent
-            placeholder="Start Date *"
-            type="date"
-            required
-            name="startDate"
-            value={startDate}
-            onChange={handleInputChange}
-          />
-          <InputComponent
-            placeholder="End Date *"
-            type="date"
-            required
-            name="endDate"
-            value={endDate}
-            onChange={handleInputChange}
-          />
-        </div>
-      </ContainerComponent>
-    </div>
-  );
-};
-
-const NewStaffComponent = ({
+const EditStaffComponent = ({
   onClose,
   onCreate
 }: {
@@ -204,13 +41,6 @@ const NewStaffComponent = ({
   const [creatingStaff, setCreatingStaff] = useState(false);
   const [newQualficationDialog, setNewQualficationDialog] = useState(false);
   const [editQualficationDialog, setEditQualficationDialog] = useState(false);
-  const [editQualficationData, setEditQualficationData] = useState({
-    _id: "",
-    qualificationName: "",
-    schoolName: "",
-    startDate: "",
-    endDate: ""
-  });
   const [localData, setLocalData] = useState({
     staffCustomId: "",
     staffFirstName: "",
@@ -373,9 +203,9 @@ const NewStaffComponent = ({
     <ContainerComponent id="staffDialogContainer" style="w-[80%] h-[90%] gap-5 overflow-auto flex flex-col">
       {newQualficationDialog && (
         <QualificationDialog
-          type="new"
+          type="edit"
           data={{
-            _id: "",
+            _id: localData.staffQualification._id,
             qualificationName: "",
             schoolName: "",
             startDate: "",
@@ -385,29 +215,11 @@ const NewStaffComponent = ({
             setNewQualficationDialog(!save);
             setLocalData((prev: any) => ({
               ...prev,
-              staffQualification: [...prev.staffQualification, returnData]
+              staffQualification: [...localData.staffQualification, returnData]
             }));
           }}
           onClose={(open) => {
             setNewQualficationDialog(!open);
-          }}
-        />
-      )}
-      {editQualficationDialog && (
-        <QualificationDialog
-          type="edit"
-          data={editQualficationData}
-          onSave={(save, returnData) => {
-            setEditQualficationDialog(!save);
-            setLocalData((prev: any) => ({
-              ...prev,
-              staffQualification: [...prev.staffQualification].map((qualification: any) =>
-                qualification._id === returnData._id ? returnData : qualification
-              )
-            }));
-          }}
-          onClose={(open) => {
-            setEditQualficationDialog(!open);
           }}
         />
       )}
@@ -650,41 +462,30 @@ const NewStaffComponent = ({
           {staffQualification.length < 1 ? (
             <div>No Qualifications Added</div>
           ) : (
-            staffQualification.map((qualification: any) => {
-              const { _id, qualificationName, schoolName, startDate, endDate } = qualification;
-              return (
-                <div
-                  key={_id}
-                  className="flex flex-col w-[300px] hover:cursor-pointer hover:bg-foregroundColor-5 border border-foregroundColor-15 rounded-lg shadow p-4"
-                  onClick={() => {
-                    setEditQualficationData(qualification);
-                    setEditQualficationDialog(true);
-                  }}
-                >
-                  <div className="flex gap-5 justify-between items-center">
-                    <span className="whitespace-nowrap font-bold text-[19px]">{qualificationName.slice(0, 20)}</span>{" "}
-                    <CgTrash
-                      className="text-[25px] hover:text-red-500"
-                      onClick={(e) => {
-                        e.stopPropagation();
-                        setUnsaved(true);
-                        setLocalData((prev: any) => ({
-                          ...prev,
-                          staffQualification: prev.staffQualification.filter(
-                            (qualification: any) => qualification._id !== _id
-                          )
-                        }));
-                      }}
-                    />
-                  </div>
-                  <span className="whitespace-nowrap font-bold text-foregroundColor-60 text-[18px]">{schoolName}</span>
-                  <div className="flex flex-col mt-2">
-                    <span>{formatDate(startDate)}</span>
-                    <span>{formatDate(endDate)}</span>
-                  </div>
+            staffQualification.map(({ _id, qualificationName, schoolName, startDate, endDate }: any) => (
+              <ContainerComponent key={_id} style="flex flex-col w-[300px] hover:cursor-pointer">
+                <div className="flex gap-5 justify-between items-center">
+                  <span className="whitespace-nowrap font-bold">{qualificationName.slice(0, 20)}</span>{" "}
+                  <CgTrash
+                    className="text-[25px] hover:text-red-500"
+                    onClick={() => {
+                      setUnsaved(true);
+                      setLocalData((prev: any) => ({
+                        ...prev,
+                        staffQualification: prev.staffQualification.filter(
+                          (qualification: any) => qualification._id !== _id
+                        )
+                      }));
+                    }}
+                  />
                 </div>
-              );
-            })
+                <span className="whitespace-nowrap font-bold text-foregroundColor-60">{schoolName}</span>
+                <div className="flex flex-col mt-2">
+                  <span>{formatDate(startDate)}</span>
+                  <span>{formatDate(endDate)}</span>
+                </div>
+              </ContainerComponent>
+            ))
           )}
         </div>
       </div>
@@ -692,4 +493,4 @@ const NewStaffComponent = ({
   );
 };
 
-export default NewStaffComponent;
+export default EditStaffComponent;
